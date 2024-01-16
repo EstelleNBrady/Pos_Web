9359d4e4952e8f8378e7acaf55cfd9e9841c3e06
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-16 16:17:07 -0600

    Add styling to main element and update header in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div>test</div>
-
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+    </main>
   );
 }
 

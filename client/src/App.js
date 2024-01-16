d14aaff5df8b4f3b6ee2cca5a1dc447f6689c9f8
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-16 17:05:21 -0600

    Refactor App component to use Header and Post components

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,14 @@
 import './App.css';
+import Header from './Header';
+import Post from './Post';
 
 function App() {
   return (
     <main>
-      <header>
-        <a href="" className="logo">MyBlog</a>
-        <nav>
-          <a href="">Login</a>
-          <a href="">Register</a>
-        </nav>
-      </header>
-
-        <div className="post">
-          <div className="image">
-            <img src="https://www.trendeing.com/wp-content/uploads/2019/04/iStock-1124754300.jpg" alt="" />
-          </div>
-          <div className="texts">
-            <h2>Body Positivity is Healthy</h2>
-            <p className="info">
-              <a className="author">Estelle Brady</a>
-              <time>2024-01-16: 16:49</time>
-            </p>
-            <p className="summary">Accepting our bodies as they are, regardless of size, shape, skin tone, gender or physical capabilities is known as body positivity. Body positivity intersects with fitness by recognizing one’s strengths and abilities and approaching fitness with an emphasis on overall health rather than just appearance.</p>
-          </div>
-        </div>
-
-        <div className="post">
-        <div className="image">
-            <img src="https://www.trendeing.com/wp-content/uploads/2019/04/iStock-1124754300.jpg" alt="" />
-          </div>
-          <div className="texts">
-            <h2>Body Positivity is Healthy</h2>
-            <p className="info">
-              <a className="author">Estelle Brady</a>
-              <time>2024-01-16: 16:49</time>
-            </p>
-            <p className="summary">Accepting our bodies as they are, regardless of size, shape, skin tone, gender or physical capabilities is known as body positivity. Body positivity intersects with fitness by recognizing one’s strengths and abilities and approaching fitness with an emphasis on overall health rather than just appearance.</p>
-          </div>
-        </div>
-
-        <div className="post">
-        <div className="image">
-            <img src="https://www.trendeing.com/wp-content/uploads/2019/04/iStock-1124754300.jpg" alt="" />
-
-          </div>
-          <div className="texts">
-            <h2>Body Positivity is Healthy</h2>
-            <p className="info">
-              <a className="author">Estelle Brady</a>
-              <time>2024-01-16: 16:49</time>
-            </p>
-            <p className="summary">Accepting our bodies as they are, regardless of size, shape, skin tone, gender or physical capabilities is known as body positivity. Body positivity intersects with fitness by recognizing one’s strengths and abilities and approaching fitness with an emphasis on overall health rather than just appearance.</p>
-          </div>
-        </div>
-
+      <Header />
+      <Post />
+      <Post />
+      <Post />
     </main>
   );
 }
---
+++ client/src/Post.js
@@ -0,0 +1,17 @@
+export default function Post(){
+    return(        <div className="post">
+    <div className="image">
+      <img src="https://www.trendeing.com/wp-content/uploads/2019/04/iStock-1124754300.jpg" alt="" />
+    </div>
+    <div className="texts">
+      <h2>Body Positivity is Healthy</h2>
+      <p className="info">
+        <a className="author">Estelle Brady</a>
+        <time>2024-01-16: 16:49</time>
+      </p>
+      <p className="summary">Accepting our bodies as they are, regardless of size, shape, skin tone, gender or physical capabilities is known as body positivity. Body positivity intersects with fitness by recognizing one’s strengths and abilities and approaching fitness with an emphasis on overall health rather than just appearance.</p>
+    </div>
+  </div>
+);
+
+}

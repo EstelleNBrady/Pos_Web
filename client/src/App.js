10734b236badbf12def63a273124f742bc7ae51c
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-16 17:16:29 -0600

    Refactor routing in App.js and index.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
 import './App.css';
-import Header from './Header';
 import Post from './Post';
+import Header from './Header';
+import {Route, Routes} from "react-router-dom";
 
 function App() {
   return (
-    <main>
-      <Header />
-      <Post />
-      <Post />
-      <Post />
-    </main>
+    <Routes>
+      <Route index element ={
+        <main>
+          <Header />
+          <Post />
+          <Post />
+          <Post />
+        </main>
+      } />
+    </Routes>
+  
   );
 }
 

939c4d4bace43492240cf1622ce7ac0f8ff746a9
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-17 23:28:01 -0600

    Set up Mongoose

--- api/index.js
+++ api/index.js
@@ -1,15 +1,20 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require("mongoose");
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+mongoose.connect('mongodb+srv://blog:ksn3TK8GYLOOxqI1@cluster0.zv4ffnn.mongodb.net/?retryWrites=true&w=majority');
+
 app.post('/register', (req,res) => {
     const {username,password} = req.body;
     res.json({requestData:{username,password}});
-
-    res.json('test ok3');
 });
 
 app.listen(4000);
+//ksn3TK8GYLOOxqI1
+//kKl4FzxEbCaFgzeC
+
+//mongodb+srv://blog:ksn3TK8GYLOOxqI1@cluster0.zv4ffnn.mongodb.net/?retryWrites=true&w=majority

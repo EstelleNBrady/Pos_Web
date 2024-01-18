6c5f440262bc10daacc11305e4998fedd3c85f48
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-17 21:19:30 -0600

    Add styling and form for registration page

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import {Route, Routes} from "react-router-dom";
 import Layout from "./Layout";
 import IndexPage from "./pages/IndexPage";
 import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage"
 
 function App() {
   return (

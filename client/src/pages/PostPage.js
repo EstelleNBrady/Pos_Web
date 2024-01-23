2da0ca79e72cafdf7f6baad5cb468867c43fed88
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-22 20:05:57 -0600

    Update App.css and PostPage.js

--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -1,9 +1,11 @@
-import {useEffect, useState} from "react";
+import {useEffect, useState, useContext} from "react";
 import {useParams} from "react-router-dom";
 import {format} from "date-fns";
+import {UserContext} from "../UserContext";
 
 export default function PostPage(){
     const [postInfo, setPostInfo] = useState(null);
+    const {userInfo} = useContext(UserContext);
     const {id} = useParams();
 
     useEffect(() => {
@@ -21,10 +23,15 @@ export default function PostPage(){
             <h1>{postInfo.title}</h1>
             <time>{format(new Date (postInfo.createdAt), 'MMM d, yyyy HH:mm')}</time>
             <div className="author">by {postInfo.author.username}</div>
+            {userInfo.id === postInfo.author._id && (
+                <div className="edit-row">
+                    <a className="edit-btn" href="">Edit this post</a>
+                    </div>
+            )}
             <div className="image">
                 <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
         </div>
-        <div dangerouslySetInnerHTML={{__html:postInfo.content}}/>
+        <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}}/>
     </div>
 
 

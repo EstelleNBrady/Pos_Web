bcb8bf6dcaf1d0b6e91b29f1020f42280f1bf376
EstelleNBrady <estellenbrady00@gmail.com>
2024-01-22 20:37:49 -0600

    Add EditPost page and update CreatePost and PostPage

--- client/src/pages/EditPost.js
+++ client/src/pages/EditPost.js
@@ -1,50 +1,26 @@
+import { useState } from "react";
+import { Navigate } from "react-router-dom";
 import ReactQuill from "react-quill";
-import 'react-quill/dist/quill.snow.css';
-import {useState} from "react";
-import {Navigate} from "react-router-dom";
-import Editor from "../Editor";
-
-
-
-
-export default function CreatePost(){
+import Editor from "../Editor"
+export default function EditPost(){
     const [title,setTitle] = useState('');
     const [summary,setSummary] = useState('');
     const [content,setContent] = useState('');
     const [files, setFiles] = useState('');
-    const [redirect, setRedirect] = useState(false);
+    const [redirect,setRedirect] = useState('');
 
-async function createNewPost(ev){
-        const data = new FormData();
+    function updatePost(ev){
         ev.preventDefault();
-        data.set('title', title);
-        data.set('summary', summary);
-        data.set('content', content);
-        data.set('file', files[0]);
-
-        ev.preventDefault();
-
-
-
-        const response=await fetch('http://localhost:4000/post', {
-            method: 'POST',
-            body: data,
-            credentials: 'include',
-        });
-
-        if(response.ok){
-            setRedirect(true);
-
-        }
-
     }
-    
     if (redirect){
         return <Navigate to={'/'} />
 
     }
+
+
+    
     return(
-        <form onSubmit={createNewPost}>
+        <form onSubmit={updatePost}>
             <input type="title"
                 placeholder={'Title'} 
                 value={title} 
@@ -56,9 +32,10 @@ async function createNewPost(ev){
 
             <input type="file"
                 onChange={ev => setFiles(ev.target.files)}/>
-            <Editor value={content} onChange={setContent}/>
+            <Editor onChange={setContent} value={content}/>
             <button style={{marginTop:'5px'}}>Create post</button>
         </form>
         
     );
 }
+
--- client/src/pages/PostPage.js
+++ client/src/pages/PostPage.js
@@ -2,6 +2,7 @@ import {useEffect, useState, useContext} from "react";
 import {useParams, Navigate} from "react-router-dom";
 import {format} from "date-fns";
 import {UserContext} from "../UserContext";
+import {Link} from "react-router-dom";
 
 export default function PostPage(){
     const [postInfo, setPostInfo] = useState(null);
@@ -25,12 +26,13 @@ export default function PostPage(){
             <div className="author">by {postInfo.author.username}</div>
             {userInfo.id === postInfo.author._id && (
                 <div className="edit-row">
-                    <a className="edit-btn" href="">
+                    <Link className="edit-btn" to={`/edit/${postInfo._id}`}>
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" />
                     </svg>
 
-                        Edit this post</a>
+                        Edit this post
+                    </Link>
                     </div>
             )}
             <div className="image">

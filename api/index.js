a907bb987e68db32e4d68e86cee1dc309bc48a11
EstelleNBrady <estellenbrady00@gmail.com>
2024-02-26 04:16:18 -0600

    Update server port in API index.js

--- api/index.js
+++ api/index.js
@@ -250,9 +250,10 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build', 'index.html'));
 });
 
-const port = process.env.PORT || 4000;
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+const PORT = process.env.PORT || 4000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
 
 

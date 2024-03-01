98ffedd073b9a2d5968a099cae6ff78632f14118
EstelleNBrady <estellenbrady00@gmail.com>
2024-02-29 23:17:42 -0600

    ENV VARIABLE

--- api/index.js
+++ api/index.js
@@ -22,7 +22,7 @@ app.use(cors({
 app.use(express.json());
 app.use(cookieParser());
 app.use('/uploads', express.static(__dirname + '/uploads'));
-mongoose.connect('mongodb+srv://blog:ksn3TK8GYLOOxqI1@cluster0.zv4ffnn.mongodb.net/?retryWrites=true&w=majority');
+mongoose.connect('process.env.MONGODB_URI');
 
 // Authentication Middleware
 const authenticateToken = (req, res, next) => {
